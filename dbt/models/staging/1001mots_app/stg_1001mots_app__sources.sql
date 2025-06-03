5980a48bcae5a4d85f3dd2246ca2ff238903e0a7
pelletgu <guillaum.pelletier@gmail.com>
2025-06-03 17:03:04 +0200

    add sources staging file

---
+++ dbt/models/staging/1001mots_app/stg_1001mots_app__sources.sql
@@ -0,0 +1,20 @@
+with source as (
+    select *
+    from {{ source('1001mots_app', 'sources') }}
+)
+
+select
+    id::string as source_id,
+    name::string as name,
+    channel::string as channel,
+    department::string as department,
+    utm::string as utm,
+    comment::string as comment,
+    to_date(
+            nullif(created_at::string, '')
+    ) as date_created,
+    to_date(
+            nullif(updated_at::string, '')
+    ) as date_updated,
+    is_archived::boolean as is_archived
+from source
