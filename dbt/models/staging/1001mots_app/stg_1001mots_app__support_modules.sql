98792b98548abdeb76f1f5e60521fc3b0401a257
pelletgu <guillaum.pelletier@gmail.com>
2025-07-17 10:20:13 +0200

    add support module staging file

---
+++ dbt/models/staging/1001mots_app/stg_1001mots_app__support_modules.sql
@@ -0,0 +1,26 @@
+with source as (
+    select *
+    from {{ source('mots_app', 'sources') }}
+)
+
+select
+    id::string as module_id,
+    name::string as name,
+    to_date(
+            nullif(created_at::string, '')
+    ) as date_created,
+    to_date(
+            nullif(updated_at::string, '')
+    ) as date_updated,
+    to_date(
+            nullif(discarded_at::string, '')
+    ) as date_discarded,
+    to_date(
+            nullif(start_at::string, '')
+    ) as date_start,
+    for_bilingual::boolean as for_bilingual,
+    theme::string as theme,
+    age_range::string as age_range,
+    level::string as level,
+    book_id::string as book_id
+from source
