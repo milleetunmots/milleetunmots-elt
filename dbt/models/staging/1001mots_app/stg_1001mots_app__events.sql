83cae0f71c892ccfa605f637a38276aed09bc4f3
pelletgu <guillaum.pelletier@gmail.com>
2025-11-25 17:29:15 +0100

    add staging files for workshops management

---
+++ dbt/models/staging/1001mots_app/stg_1001mots_app__events.sql
@@ -0,0 +1,26 @@
+with source as (
+    select *
+    from {{ source('mots_app', 'events') }}
+)
+
+select
+    id::string as event_id,
+    workshop_id::string as workshop_id,
+    related_type::string as related_type,
+    related_id::int as related_id,
+    to_date(
+            nullif(created_at::string, '')
+    ) as date_created,
+    to_date(
+            nullif(occurred_at::string, '')
+    ) as date_occurred,
+    to_date(
+            nullif(acceptation_date::string, '')
+    ) as date_accepted,
+    to_date(
+            nullif(discarded_at::string, '')
+    ) as date_discarded,
+    body::string as body,
+    parent_response::string as parent_response,
+    parent_presence::string as parent_presence
+from source
---
+++ dbt/models/staging/1001mots_app/stg_1001mots_app__workshops.sql
@@ -0,0 +1,27 @@
+with source as (
+    select *
+    from {{ source('mots_app', 'workshops') }}
+)
+
+select
+    id::string as workshop_id,
+    name::string as workshop_name,
+    workshop_land::string as workshop_land,
+    to_date(
+            nullif(created_at::string, '')
+    ) as date_created,
+    to_date(
+            nullif(updated_at::string, '')
+    ) as date_updated,
+    to_date(
+            nullif(workshop_date::string, '')
+    ) as date_workshop,
+    to_date(
+            nullif(discarded_at::string, '')
+    ) as date_discarded,
+	topic::string as topic,
+	co_animator::string as co_animator,
+	animator_id::int as animator_id,
+    postal_code::string as postal_code,
+    city_name::string as city_name
+from source
