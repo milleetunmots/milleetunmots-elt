687d5f27eb82e7a099a5788394683f74d5da297f
pelletgu <guillaum.pelletier@gmail.com>
2026-01-20 22:27:24 +0100

    add sms table

---
+++ dbt/models/fact/sms.sql
@@ -0,0 +1,25 @@
+with events as (
+    select *
+    from {{ ref('stg_1001mots_app__events') }}
+    where type = 'Events::TextMessage'
+),
+
+pe as (
+    select *
+    from {{ ref('parent_enfant') }}
+),
+
+mecene as (
+    select *
+    from {{ ref('mecene') }}
+)
+
+select
+    mecene.*,
+    events.date_occurred,
+    events.body
+from events
+left join pe
+    on events.related_id = pe.parent_id
+left join mecene
+    on pe.youngest_child_id = mecene.child_id
